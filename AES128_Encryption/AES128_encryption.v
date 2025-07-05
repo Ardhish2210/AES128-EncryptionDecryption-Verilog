248ca2a96de269b5d97cc46aaa80685fc8293b2e
Ardhish Patel <ardhishp@gmail.com>
2025-07-05 11:57:09 +0530

    AES128_encryption.v

--- AES128_Encryption/AES128_encryption.v
+++ AES128_Encryption/AES128_encryption.v
@@ -5,9 +5,6 @@
 `include "shift_rows.v"
 `include "mix_columns.v"
 
-//`include "aes_sbox.v"
-//`include "sub_words.v"
-
 module AES128_encryption (
   input  wire [127:0] plaintext,
   input  wire [127:0] key,
@@ -18,38 +15,24 @@ module AES128_encryption (
   wire [127:0] round_keys[0:10];
   genvar i;
 
-  // Get all round keys
+
+  // The below loop will help us fetch all the round keys
   generate
     for (i = 0; i <= 10; i = i + 1) begin : keys
-      key_expansion KE (
-        .key(key),
-        .round(i[3:0]),
-        .round_key(round_keys[i])
-      );
+      key_expansion KE (.key(key), .round(i[3:0]), .round_key(round_keys[i]));
     end
   endgenerate
 
-  // Initial AddRoundKey (Round 0)
+  // This is the key used for INITIAL TRANSFORMSTION
   assign state[0] = plaintext ^ round_keys[0];
 
-  // Rounds 1 to 9
+  // Transformation's from ROUND-1 to ROUND-9
   generate
     for (i = 1; i < 10; i = i + 1) begin : rounds
-      encrypt_round ROUND (
-        .in(state[i-1]),
-        .key(round_keys[i]),
-        .is_final_round(1'b0),
-        .out(state[i])
-      );
+      encrypt_round ROUND (.in(state[i-1]), .key(round_keys[i]), .is_final_round(1'b0), .out(state[i]));
     end
   endgenerate
 
-  // Final Round (10)
-  encrypt_round FINAL (
-    .in(state[9]),
-    .key(round_keys[10]),
-    .is_final_round(1'b1),
-    .out(ciphertext)
-  );
-
+  // ROUND-10 (Final) Transformation
+  encrypt_round FINAL (.in(state[9]), .key(round_keys[10]), .is_final_round(1'b1), .out(ciphertext));
 endmodule
